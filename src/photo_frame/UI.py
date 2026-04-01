b62812857c4d0c6efd526981ddbaea734f55403b
echo <edward@eparry.uk>
2026-04-01 23:10:17 +0100

    Tried to add the tkinter stuff to get this to work as a service

--- src/photo_frame/UI.py
+++ src/photo_frame/UI.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 import PySimpleGUI as sg
 from PIL import Image, ExifTags, ImageOps
@@ -13,6 +14,10 @@ G_SIZE = (1920, 1080)          # Size of the Graph in pixels. Using a 1 to 1 map
 
 sg.theme('black')
 register_heif_opener()
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 
 def normalise_rotation_tiff(image: Image.Image):
